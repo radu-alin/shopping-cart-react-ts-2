8a5d2189081f15e663dd6107b37c1052fb0ea312
Alin RADU <cristian.alin.radu@reea.net>
2022-08-26 12:16:11 +0300

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App"><Button>Hello</Button></div>
+    <div className="App">
+      <Button>Hello</Button>
+    </div>
   );
 };
 
